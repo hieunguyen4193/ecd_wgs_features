120d65f8643ef5beacb2dabe1048ed408e584b36
hieunguyen4193 <work@hieunguyen.de>
2024-07-18 13:36:40 +0200

    update extract fragments coords

---
+++ extract_fragment_coords_from_bam.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+# inputbam=$1
+# outputdir=$2
+# num_threads=$3
+
+# get samtools to work
+export PATH=/Users/hieunguyen/samtools/bin:$PATH 
+# samtools --version
+
+# default input values
+inputbam="/Volumes/HNSD02/data/WGS_bam/9-ZMC014NB_S95025-S97025.sorted.bam";
+outputdir="/Volumes/HNSD02/outdir/ecd_wgs_features";
+path_to_fa="/Volumes/HNSD02/resource/hg19.fa";
+num_threads=10;
+nucleosome_ref="/Volumes/HNSD02/resource/nucleosome_footprint_bed/rpr_map_Budhraja_STM2023.bed"
+
+mkdir -p ${outputdir};
+
+# Parse command line arguments
+while getopts "i:o:t:" opt; do
+  case ${opt} in
+    i )
+      inputbam=$OPTARG
+      ;;
+    o )
+      outputdir=$OPTARG
+      ;;
+    t )
+      num_threads=$OPTARG
+      ;;
+    \? )
+      echo "Usage: cmd [-i] inputbam [-o] outputdir [-t] num_threads"
+      exit 1
+      ;;
+  esac
+done
+
+echo -e "input bam file: " ${inputbam}
+# Check if the input BAM file exists
+if [ ! -f "${inputbam}" ]; then
+    echo "Input BAM file does not exist: ${inputbam}"
+    exit 1
+fi
+
+filename=$(echo ${inputbam} | xargs -n 1 basename)
+filename=${filename%.bam*}
+
+samtools view ${inputbam}| cut -f1,3,4,9 \
+| awk -v OFS='\t' '{if ($4 > 0){$5=$3+$4; print $0}}' > ${outputdir}/${filename}.coord.bed
+    
