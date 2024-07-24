0f101120fd20b74dcd0227ffb6b998dffae5b78d
hieunguyen4193 <work@hieunguyen.de>
2024-07-25 00:12:59 +0200

    check update

---
+++ helper_functions.R
@@ -0,0 +1,23 @@
+`%ni%` = Negate(`%in%`)
+
+#####----------------------------------------------------------------------#####
+# function to create an interactive data table
+#####----------------------------------------------------------------------#####
+create_dt <- function(x){
+  DT::datatable(x,
+                extensions = 'Buttons',
+                filter = "top",
+                options = list(dom = 'Blfrtip',
+                               buttons = c('copy', 'csv', 'excel', 'pdf', 'print'),
+                               lengthMenu = list(c(10,25,50,-1),
+                                                 c(10,25,50,"All")),
+                               columnDefs = list(list(
+                                 targets = "_all",
+                                 render = JS(
+                                   "function(data, type, row, meta) {",
+                                   "return type === 'display' && data != null && data.length > 100 ?",
+                                   "'<span title=\"' + data + '\">' + data.substr(0, 100) + '...</span>' : data;",
+                                   "}")
+                               ))
+                ))
+}
