722a05ef4f1234aa139e8fbbf1c2728daee97f10
hieunguyen4193 <work@hieunguyen.de>
2024-09-27 14:27:38 +0200

    update

--- run_to_generate_HTML_QC_report.R
+++ run_to_generate_HTML_QC_report.R
@@ -1,10 +1,31 @@
 gc()
 rm(list = ls())
 
-path.to.qc.report <- "/home/hieunguyen/CRC1382/src/ecd_wgs_features/summary_QC.Rmd"
-html.name <- "test.html"
-path.to.save.html.report <- "/home/hieunguyen/CRC1382/src/ecd_wgs_features/output/QC"
-read_length <- 50
+library(argparse)
+
+parser <- ArgumentParser()
+
+parser$add_argument("-i", "--input", action="store",
+                    help="Path to input QC folder containing all samples in the batch")
+parser$add_argument("-o", "--output", action="store",
+                    help="Path to save all output")
+parser$add_argument("-n", "--html_name", action="store",
+                    help="Name of the output html file")
+parser$add_argument("-r", "--read_length", action="store",
+                    help="Path to save output html file")
+
+args <- parser$parse_args()
+
+# path.to.qc.report <- "/home/hieunguyen/CRC1382/src/ecd_wgs_features/summary_QC.Rmd"
+# html.name <- "test.html"
+# path.to.save.html.report <- "/home/hieunguyen/CRC1382/src/ecd_wgs_features/output/QC"
+# read_length <- 50
+
+path.to.qc.report <- args$input
+html.name <- args$batch_name
+path.to.save.html.report <- args$output
+read_length <- args$read_length
+
 rmarkdown::render(input = path.to.qc.report, 
                   params = list(
                     input = "/home/hieunguyen/CRC1382/src/ecd_wgs_features/output/QC",
