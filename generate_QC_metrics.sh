d6b66ea648758fd7324b447ee1a87cd289491ef7
hieunguyen4193 <work@hieunguyen.de>
2024-09-27 11:12:42 +0200

    update

--- generate_QC_metrics.sh
+++ generate_QC_metrics.sh
@@ -2,7 +2,7 @@
 export PATH=/Users/hieunguyen/samtools/bin:$PATH
 
 # note for HieuNguyen: on your macM3 do not use the conda env pytorch, use the base env. It has java 23.
-# bash generate_QC_metrics.sh -i /Users/hieunguyen/src/ecd_wgs_features/output/WGShg19.sorted.markdup.bam -o ./output -n 10 -f /Users/hieunguyen/data/resources/hg19.fa
+# bash generate_QC_metrics.sh -i /Users/hieunguyen/src/ecd_wgs_features/output/WGShg19.sorted.bam -o ./output -n 10 -f /Users/hieunguyen/data/resources/hg19.fa -m true
 
 #####----------------------------------------------------------------------#####
 ##### input args
@@ -50,7 +50,7 @@ if [ "${rerun_markdup}" = "true" ]; then
     java -Xms512m -Xmx4g -jar ./picard.jar MarkDuplicates \
     I=${inputbam} \
     O=${outputdir}/${sampleid}.sorted.markdup.bam \
-    M=${outputdir}/QC/${sampleid}.marked_dup_metrics.txt
+    M=${outputdir}/QC/${sampleid}/${sampleid}.marked_dup_metrics.txt
 
     # replace inputbam by the new markdup bam
     inputbam=${outputdir}/${sampleid}.sorted.markdup.bam
@@ -69,16 +69,16 @@ fi
 #####----------------------------------------------------#####
 
 # samtools flagstat
-mkdir -p ${outputdir}/QC;
+mkdir -p ${outputdir}/QC/${sampleid};
 echo -e "runnning samtools flagstats"
-samtools flagstat ${inputbam} -@ ${samtools_num_threads} --output-fmt 'tsv' > ${outputdir}/QC/${sampleid}.flagstat.txt
+samtools flagstat ${inputbam} -@ ${samtools_num_threads} --output-fmt 'tsv' > ${outputdir}/QC/${sampleid}/${sampleid}.flagstat.txt
 echo -e "finished"
 
 # samtools 
 echo -e "running samtools stats"
-samtools stat ${inputbam} -@ ${samtools_num_threads} > ${outputdir}/QC/${sampleid}.sortedbamfilename.stats
-plot-bamstats -p ${outputdir}/QC/samtools_stat_plots/${sampleid} ${outputdir}/QC/${sampleid}.sortedbamfilename.stats
-rm -rf ${outputdir}/QC/samtools_stat_plot/*.gp
+samtools stat ${inputbam} -@ ${samtools_num_threads} > ${outputdir}/QC/${sampleid}/${sampleid}.sortedbamfilename.stats
+plot-bamstats -p ${outputdir}/QC/${sampleid}/samtools_stat_plots/${sampleid} ${outputdir}/QC/${sampleid}/${sampleid}.sortedbamfilename.stats
+rm -rf ${outputdir}/QC/${sampleid}/samtools_stat_plot/*.gp
 echo -e "finished"
 
 # Some picard QC metrics
@@ -86,29 +86,29 @@ echo -e "running picard collect alignment summary metrics"
 java -Xms512m -Xmx4g -jar ./picard.jar CollectAlignmentSummaryMetrics \
     R=${ref} \
     I=${inputbam} \
-    O=${outputdir}/QC/${sampleid}.alignment_summary_metrics.txt
+    O=${outputdir}/QC/${sampleid}/${sampleid}.alignment_summary_metrics.txt
 echo -e "finished"
 
 echo -e "running collect insert size metrics";
 java -Xms512m -Xmx4g -jar ./picard.jar CollectInsertSizeMetrics \
     I=${inputbam} \
-    O=${outputdir}/QC/${sampleid}.insert_size_metrics.txt \
-    H=${outputdir}/QC/${sampleid}.insert_size_histogram.pdf
+    O=${outputdir}/QC/${sampleid}/${sampleid}.insert_size_metrics.txt \
+    H=${outputdir}/QC/${sampleid}/${sampleid}.insert_size_histogram.pdf
 echo -e "finished"
 
 echo -e "running collect gc bias metrics";″
 java -Xms512m -Xmx4g -jar ./picard.jar CollectGcBiasMetrics \
     R=${ref} \
     I=${inputbam} \
-    O=${outputdir}/QC/${sampleid}.gc_bias_metrics.txt \
-    CHART=${outputdir}/QC/${sampleid}.gc_bias_metrics.pdf \
-    S=${outputdir}/QC/${sampleid}.summary_metrics.txt
+    O=${outputdir}/QC/${sampleid}/${sampleid}.gc_bias_metrics.txt \
+    CHART=${outputdir}/QC/${sampleid}/${sampleid}.gc_bias_metrics.pdf \
+    S=${outputdir}/QC/${sampleid}/${sampleid}.summary_metrics.txt
 echo -e "finished"
 
 echo -e "running collect gc bias metrics";″
 java -Xms512m -Xmx4g -jar ./picard.jar CollectWgsMetrics \
     R=${ref} \
     I=${inputbam} \
-    O=${outputdir}/QC/${sampleid}.wgs_metrics.txt
+    O=${outputdir}/QC/${sampleid}/${sampleid}.wgs_metrics.txt
 echo -e "finished"
 
