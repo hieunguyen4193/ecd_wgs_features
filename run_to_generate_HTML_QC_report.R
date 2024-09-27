5270d349752827607ced3846feb25091e63bab75
hieunguyen4193 <work@hieunguyen.de>
2024-09-27 14:28:20 +0200

    update

--- run_to_generate_HTML_QC_report.R
+++ run_to_generate_HTML_QC_report.R
@@ -15,16 +15,17 @@ parser$add_argument("-r", "--read_length", action="store",
                     help="Path to save output html file")
 
 args <- parser$parse_args()
+path.to.qc.report <- args$input
+html.name <- args$batch_name
+path.to.save.html.report <- args$output
+read_length <- args$read_length
 
 # path.to.qc.report <- "/home/hieunguyen/CRC1382/src/ecd_wgs_features/summary_QC.Rmd"
 # html.name <- "test.html"
 # path.to.save.html.report <- "/home/hieunguyen/CRC1382/src/ecd_wgs_features/output/QC"
 # read_length <- 50
 
-path.to.qc.report <- args$input
-html.name <- args$batch_name
-path.to.save.html.report <- args$output
-read_length <- args$read_length
+
 
 rmarkdown::render(input = path.to.qc.report, 
                   params = list(
