6a7e956db0c14a1b63c1e1dba05ba5491a5d164f
hieunguyen4193 <work@hieunguyen.de>
2024-09-29 22:50:43 +0200

    update

--- 02_calculate_EM_FLEN_NUC_from_FRAG.sh
+++ 02_calculate_EM_FLEN_NUC_from_FRAG.sh
@@ -113,8 +113,8 @@ if [ ! -f "${outputdir}/${sampleid}.finished_Nucleosome.txt" ]; then
   cat ${outputdir}/${sampleid}.forward_Nucleosome.bed | awk '{if($5 > 0) print $0}' > ${outputdir}/${sampleid}.full_Nucleosome.bed
   cat ${outputdir}/${sampleid}.reverse_Nucleosome.bed | awk '{if($5 <= 0) print $0}' >> ${outputdir}/${sampleid}.full_Nucleosome.bed
   python convert_full_bed_nucleosome.py ${outputdir}/${sampleid}.full_Nucleosome.bed ${outputdir}/${sampleid}.full_Nucleosome.sorted.bed
-  bedtools closest -a ${outputdir}/${sampleid}.full_Nucleosome.sorted.bed -b ${nucleosome_ref} | cut -f1,2,11 > ${outputdir}/${sampleid}.full_Nucleosome.dist.bed
-  awk -v OFS='\t' '{$4=$3-$2; print $0}' ${outputdir}/${sampleid}.full_Nucleosome.dist.bed > ${outputdir}/${sampleid}.
+  bedtools closest -a ${outputdir}/${sampleid}.full_Nucleosome.sorted.bed -b ${nucleosome_ref} | cut -f1,2,12 > ${outputdir}/${sampleid}.full_Nucleosome.dist.bed
+  awk -v OFS='\t' '{$4=$3-$2; print $0}' ${outputdir}/${sampleid}.full_Nucleosome.dist.bed > ${outputdir}/${sampleid}.full_Nucleosome.dist.final.bed
   
   #####
 
