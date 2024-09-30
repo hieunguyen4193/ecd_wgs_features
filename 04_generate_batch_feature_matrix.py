02f178fb5ecef2a5ad9da71b08c504f9967b8c90
hieunguyen4193 <work@hieunguyen.de>
2024-09-30 13:05:50 +0200

    update

---
+++ 04_generate_batch_feature_matrix.py
@@ -0,0 +1,29 @@
+import argparse
+import pandas as pd 
+import os
+from feature_class import *
+##### Feature class definition
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--input', type=str, required=True, help='Path to batch output folder, a folder containing many sample, 1 sample per folder')
+    parser.add_argument('--output', type=str, required=False, help='Path to save output feature csv files')
+    
+    args = parser.parse_args()
+    input_batch = args.input
+    outputdir = args.output
+    
+    batch_name = os.path.basename(input_batch)
+    all_features = WGS_GW_features(path_to_feature_dir=input_batch,
+                               path_to_metadata=None)
+
+    emdf = all_features.generate_em_matrix().reset_index()
+    nucdf = all_features.generate_nuc_matrix().reset_index()
+    flendf = all_features.generate_flen_matrix().reset_index()
+    os.system(f"mkdir -p {outputdir}/{batch_name}")
+    
+    emdf.to_csv(os.path.join(outputdir, batch_name, 'EM.csv'), index = None)
+    flendf.to_csv(os.path.join(outputdir, batch_name, 'FLEN.csv'), index = None)
+    nucdf.to_csv(os.path.join(outputdir, batch_name, 'NUCLEOSOME.csv'), index = None)
+
+if __name__ == '__main__':
+    main()
--- run_pipeline.sh
+++ run_pipeline.sh
@@ -1,13 +1,44 @@
-bash 01_BAM_to_FRAGS.sh \
-    -i /Users/hieunguyen/data/tmp/debug_ecd_wgs_features/data/1-ZLAAO90NB_S7509-S7709.sorted.bam \
-    -o ./output_debug/ \
-    -n 10 \
-    -f /Users/hieunguyen/data/resources/hg19.fa;
+inputbam="/Users/hieunguyen/data/tmp/debug_ecd_wgs_features/data/1-ZLAAO90NB_S7509-S7709.sorted.bam";
+outputdir="./batch_test/";
+num_threads=10;
+path_to_fa="/Users/hieunguyen/data/resources/hg19.fa";
+path_to_ref="/Users/hieunguyen/data/resources/rpr_map_EXP0779.sorted.bed";
+motif_order_path="./motif_order.csv";
+final_Feature_dir="./final_Feature_dir";
 
-bash 02_calculate_EM_FLEN_NUC_from_FRAG.sh \
-    -i ./output_debug/1-ZLAAO90NB_S7509-S7709.sorted/1-ZLAAO90NB_S7509-S7709.sorted.frag.tsv  \
-    -o ./output_debug/ \
-    -f /Users/hieunguyen/data/resources/hg19.fa \
-    -r /Users/hieunguyen/data/resources/rpr_map_EXP0779.sorted.bed \
-    -c false;
+mkdir -p ${outputdir};
+mkdir -p ${final_Feature_dir};
+
+sampleid=$(echo ${inputbam} | xargs -n 1 basename)
+sampleid=${sampleid%.bam*}
+
+if [ ! -f "${outputdir}/${sampleid}/${sampleid}.final_output.tsv" ]; then
+    bash 01_BAM_to_FRAGS.sh \
+    -i  ${inputbam} \
+    -o ${outputdir} \
+    -n ${num_threads} \
+    -f ${path_to_fa};
+
+    bash 02_calculate_EM_FLEN_NUC_from_FRAG.sh \
+        -i ${outputdir}/${sampleid}/${sampleid}.frag.tsv  \
+        -o ${outputdir} \
+        -f ${path_to_fa} \
+        -r ${path_to_ref} \
+        -c false;
+else 
+    echo -e ${outputdir}/${sampleid}/${sampleid}.final_output.tsv "exists";
+    echo -e "Running script 03 to generate features ..."
+    python 03_generate_WGS_features.py \
+        --input ${outputdir}/${sampleid}/${sampleid}.final_output.tsv \
+        --output ${outputdir}/${sampleid} \
+        --motif_order_path ${motif_order_path}
+
+    echo -e "Finished generating features, saving csv files"
+    python 04_generate_batch_feature_matrix.py \
+        --input ${outputdir} \
+        --output ${final_Feature_dir} \
+        --motif_order_path ${motif_order_path}
+fi
+
+
 
