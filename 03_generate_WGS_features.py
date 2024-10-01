17a38c9bf2928fa1da17970422235bdcbe9cba94
hieunguyen4193 <work@hieunguyen.de>
2024-10-01 11:01:11 +0200

    update

--- 03_generate_WGS_features.py
+++ 03_generate_WGS_features.py
@@ -8,15 +8,18 @@ def main():
     parser.add_argument('--input', type=str, required=True, help='Path to the pre-processed frag.tsv files from 01 and 02')
     parser.add_argument('--output', type=str, required=False, help='Path to save output feature csv files')
     parser.add_argument('--motif_order_path', type=str, required=True, help='Path to the motif order file')
+    parser.add_argument('--feature_version', type=str, required=True, help='Name of the feature version')
     
     args = parser.parse_args()
     input_tsv = args.input
     motif_order_path = args.motif_order_path 
     outputdir = args.output
+    feature_version = args.feature_version
     
     output_obj = WGS_GW_Image_features(input_tsv = input_tsv,
                              motif_order_path = motif_order_path,
-                             outputdir = outputdir)
+                             outputdir = outputdir,
+                             feature_version = feature_version)
     
     ##### generate GW features and save features to output dir
     output_obj.generate_flen_feature()
