1e04078aecba2e00cf937a3f1ae20323af9b8a2c
hieunguyen4193 <work@hieunguyen.de>
2024-10-12 19:33:49 +0200

    update

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -13,11 +13,11 @@ sampleid=$(echo ${inputbam} | xargs -n 1 basename)
 sampleid=${sampleid%.bam*}
 
 if [ ! -f "${outputdir}/${sampleid}/${sampleid}.final_output.tsv" ]; then
-    # bash 01_BAM_to_FRAGS.sh \
-    # -i  ${inputbam} \
-    # -o ${outputdir} \
-    # -n ${num_threads} \
-    # -f ${path_to_fa};
+    bash 01_BAM_to_FRAGS.sh \
+    -i  ${inputbam} \
+    -o ${outputdir} \
+    -n ${num_threads} \
+    -f ${path_to_fa};
 
     bash 02_calculate_EM_FLEN_NUC_from_FRAG.sh \
         -i ${outputdir}/${sampleid}/${sampleid}.frag.tsv  \
@@ -25,6 +25,21 @@ if [ ! -f "${outputdir}/${sampleid}/${sampleid}.final_output.tsv" ]; then
         -f ${path_to_fa} \
         -r ${path_to_ref} \
         -c false;
+        
+    echo -e ${outputdir}/${sampleid}/${sampleid}.final_output.tsv "exists";
+    echo -e "Running script 03 to generate features ..."
+    python 03_generate_WGS_features.py \
+        --input ${outputdir}/${sampleid}/${sampleid}.final_output.tsv \
+        --output ${outputdir}/${sampleid} \
+        --motif_order_path ${motif_order_path} \
+        --feature_version "20241001" \
+        --old_nuc ${outputdir}/${sampleid}/${sampleid}.full_Nucleosome.dist.final.bed \
+        --generate_feature "all" 
+
+    echo -e "Finished generating features, saving csv files"
+    python 04_generate_batch_feature_matrix.py \
+        --input ${outputdir} \
+        --output ${final_Feature_dir}
 else 
     echo -e ${outputdir}/${sampleid}/${sampleid}.final_output.tsv "exists";
     echo -e "Running script 03 to generate features ..."
