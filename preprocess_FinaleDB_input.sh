c1446b0c2200bbf326be1b88a46d8d8ba227124a
hieunguyen4193 <work@hieunguyen.de>
2024-10-12 17:25:01 +0200

    update

--- preprocess_FinaleDB_input.sh
+++ preprocess_FinaleDB_input.sh
@@ -19,8 +19,14 @@ mkdir -p ${outputdir};
 
 echo -e "Working on the file " ${sampleid} " at the link " ${inputfrag}
 
-cat ${inputfrag} | awk -v OFS='\t' '{$5 = $1"_"$2"_"$3"_"$4"_"$5;$6 = $4;$4 = $3 - $2; if ($2 > 5){print $0}}' > ${outputdir}/${sampleid}.tmp.frag.tsv
+# make the following modification on the column of FinaleDB input file
+# 1. Add "chr" to the first column
+# 2. Add a new column at $5 (readID) by concatenating the first 5 columns.
+# 3. Move column 4 (read mappping quality) to column 6
+# 4. Replace column 4 (read length) with the fragment length (column 3 - column 2)
+# 5. Filter out rows where the second column (fragment start) is less than 5, avoid NaN in the output in calculating End motif 4bp
+# 6. Keep only rows where the first column is in 1-22, X, Y, or MT
 
-# Filter rows where the first column is in 1-22, X, Y, or MT
+cat ${inputfrag} | awk -v OFS='\t' '{$1="chr"$1;$5 = $1"_"$2"_"$3"_"$4"_"$5;$6 = $4;$4 = $3 - $2; if ($2 > 5){print $0}}' > ${outputdir}/${sampleid}.tmp.frag.tsv
 awk '$1 ~ /^([1-9]|1[0-9]|2[0-2]|X|Y|MT)$/' ${outputdir}/${sampleid}.tmp.frag.tsv > ${outputdir}/${sampleid}.FinaleDB.frag.tsv
 rm -rf ${inputfrag%.frag*}.tmp.frag.tsv
--- test.sh
+++
@@ -1,7 +0,0 @@
-inputfrag="/Users/hieunguyen/data/FinaleDB/EE87519.hg19.frag.tsv";
-path_to_fa="/Users/hieunguyen/data/resources/hg19_no_chr.fa";
-cat ${inputfrag} | \
-      awk '{start=$2 - 1; end= $2 - 1 + 4; name= $5; strand = "+"; print $1 "\t" start "\t" end "\t" name "\t" "1" "\t" strand}' \
-    > test.forward_endcoord4bp.bed;
-
-bedtools getfasta -s -name -tab -fi ${path_to_fa} -bed test.forward_endcoord4bp.bed 
